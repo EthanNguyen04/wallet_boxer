01ee439229c0409349a56e4e09b7a3b393809f14
ThangProDev <148971013+ThangProDev@users.noreply.github.com>
2024-10-31 15:18:30 +0700

    Ethan done full UI wallet

--- src/app/(account)/import_account/page.tsx
+++ src/app/(account)/import_account/page.tsx
@@ -12,21 +12,9 @@ export default function ImportWallet() {
     return (
         <div 
         style={{
-            background: 'linear-gradient(to bottom, #464660 -5%, #14131E 30%, #14131E 50%, #14131E 60%, #14131E 100%)', // Gradient với 4 màu
-        }}  className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <header className="row-start-1 items-center justify-center mt-20">
-                <Image
-                    className="dark:invert mt-10"
-                    src={icon}
-                    alt="Boxer logo"
-                    width={100}
-                    height={100}
-                    />
-            </header>
-
-            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <RestoreWallet/>
-            </main>
+            background: 'linear-gradient(to bottom, #464660 -5%, #14131E 60%, #14131E 60%, #14131E 100%)', // Gradient với 4 màu
+            }}  className="flex flex-col min-h-screen p-5 tracking-tighter font-bold">
+            <RestoreWallet/>
         </div>
     );
 }
